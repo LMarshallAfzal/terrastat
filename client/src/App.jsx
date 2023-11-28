f31900c909ec0f9dec324d82a1cb7096c93589f2
Leonard Marshall Afzal <loma256@gmail.com>
2023-11-28 15:27:04 +0000

    Implement event handlers for pointer events on country polygons
    - Add a click event handler to change colour of polygon when clicked
    - Add a mouseover event handler to change style of polygon when mouse
      hovers over it
    - Add a mouseout event handler to change style of polygon back to
      original when mouse moves away

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-// import { useState } from "react";
+import { useState } from "react";
 import "./App.css";
 import "leaflet/dist/leaflet.css";
 import "leaflet/dist/leaflet";
@@ -6,6 +6,8 @@ import countriesData from "./data/countries.js";
 import { MapContainer, TileLayer, Polygon } from "react-leaflet";
 
 function App() {
+  const [clickedPolygonId, setClickedPolygonId] = useState(null);
+
   return (
     <MapContainer center={[51.505, -0.09]} zoom={4}>
       <TileLayer
@@ -18,18 +20,53 @@ function App() {
           item[0],
         ]);
 
+        const isClicked = clickedPolygonId === country;
+
         return (
           <Polygon
             key={country}
             pathOptions={{
-              fillColor: "#000",
-              fillOpacity: 0.7,
-              weight: 5,
-              opacity: 1,
-              dashArray: 3,
-              color: "black",
+              fillColor: isClicked ? "#fa0000" : "#000",
+              fillOpacity: isClicked ? 1 : 0.7,
+              weight: isClicked ? 5 : 3,
+              opacity: isClicked ? 1 : 0.7,
+              dashArray: isClicked ? "" : 3,
+              color: isClicked ? "#807c7c" : "black",
             }}
             positions={coordinates}
+            eventHandlers={{
+              click: () => {
+                if (!isClicked) {
+                  setClickedPolygonId(country);
+                } else {
+                  setClickedPolygonId(null);
+                }
+              },
+              mouseover: (e) => {
+                if (!isClicked) {
+                  const layer = e.target;
+                  layer.setStyle({
+                    fillOpacity: 0.7,
+                    weight: 5,
+                    dashArray: "",
+                    color: "#807c7c",
+                    fillColor: "#ffffff",
+                  });
+                }
+              },
+              mouseout: (e) => {
+                if (!isClicked) {
+                  const layer = e.target;
+                  layer.setStyle({
+                    fillOpacity: 0.7,
+                    weight: 3,
+                    dashArray: 3,
+                    color: "black",
+                    fillColor: "#000",
+                  });
+                }
+              },
+            }}
           />
         );
       })}
