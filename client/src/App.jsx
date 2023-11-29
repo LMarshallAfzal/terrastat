9a719bef427d49d668dcc85e034b37d97b190d82
Leonard Marshall Afzal <loma256@gmail.com>
2023-11-29 00:42:25 +0000

    Implement a sidebar the moves over the top of the map-container

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,72 +11,107 @@ const northeastCorner = latLng([85.0511, 180]);
 const bounds = latLngBounds(southwestCorner, northeastCorner);
 
 function App() {
-  const [clickedPolygonId, setClickedPolygonId] = useState(null);
+  const [clickedCountry, setClickedCountry] = useState(null);
+  const [sidebarIsOpen, setSidebarIsOpen] = useState(false);
+
+  const handleCountryClick = (isClicked, country) => {
+    if (isClicked) {
+      setClickedCountry(null);
+      setSidebarIsOpen(false);
+    } else {
+      setClickedCountry(country);
+      setSidebarIsOpen(true);
+    }
+    console.log(country);
+  };
+
+  const handleSidebarClose = () => {
+    setClickedCountry(null);
+    setSidebarIsOpen(false);
+  };
 
   return (
-    <MapContainer maxBounds={bounds} maxZoom={18} minZoom={3} center={[51.505, -0.09]} zoom={4}>
-      <TileLayer
-        attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://api.maptiler.com/maps/basic-v2/{z}/{x}/{y}.png?key=2K46SsHb5CdTvQzDyjPq"
-      />
-
-      {countriesData.features.map((country) => {
-        const coordinates = country.geometry.coordinates[0].map((item) => [
-          item[1],
-          item[0],
-        ]);
-
-        const isClicked = clickedPolygonId === country;
-
-        return (
-          <Polygon
-            key={country}
-            pathOptions={{
-              fillColor: isClicked ? "#fa0000" : "#000",
-              fillOpacity: isClicked ? 1 : 0.7,
-              weight: isClicked ? 5 : 3,
-              opacity: isClicked ? 1 : 0.7,
-              dashArray: isClicked ? "" : 3,
-              color: isClicked ? "#807c7c" : "black",
-            }}
-            positions={coordinates}
-            eventHandlers={{
-              click: () => {
-                if (!isClicked) {
-                  setClickedPolygonId(country);
-                } else {
-                  setClickedPolygonId(null);
-                }
-              },
-              mouseover: (e) => {
-                if (!isClicked) {
-                  const layer = e.target;
-                  layer.setStyle({
-                    fillOpacity: 0.7,
-                    weight: 5,
-                    dashArray: "",
-                    color: "#807c7c",
-                    fillColor: "#ffffff",
-                  });
-                }
-              },
-              mouseout: (e) => {
-                if (!isClicked) {
-                  const layer = e.target;
-                  layer.setStyle({
-                    fillOpacity: 0.7,
-                    weight: 3,
-                    dashArray: 3,
-                    color: "black",
-                    fillColor: "#000",
-                  });
-                }
-              },
-            }}
+    <div className="App">
+      <div
+        className="map-container"
+        style={{
+          zIndex: 10,
+        }}
+      >
+        <MapContainer
+          maxBounds={bounds}
+          maxZoom={18}
+          minZoom={3}
+          center={[51.505, -0.09]}
+          zoom={4}
+        >
+          <TileLayer
+            attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://api.maptiler.com/maps/basic-v2/{z}/{x}/{y}.png?key=2K46SsHb5CdTvQzDyjPq"
           />
-        );
-      })}
-    </MapContainer>
+          {countriesData.features.map((country) => {
+            const coordinates = country.geometry.coordinates[0].map((item) => [
+              item[1],
+              item[0],
+            ]);
+
+            const isClicked = clickedCountry === country;
+
+            return (
+                <Polygon
+                  key={country}
+                  pathOptions={{
+                    fillColor: isClicked ? "#fa0000" : "#000",
+                    fillOpacity: isClicked ? 1 : 0.7,
+                    weight: isClicked ? 5 : 3,
+                    opacity: isClicked ? 1 : 0.7,
+                    dashArray: isClicked ? "" : 3,
+                    color: isClicked ? "#807c7c" : "black",
+                  }}
+                  positions={coordinates}
+                  eventHandlers={{
+                    click: () => handleCountryClick(isClicked, country),
+                    mouseover: (e) => {
+                      if (!isClicked) {
+                        const layer = e.target;
+                        layer.setStyle({
+                          fillOpacity: 0.7,
+                          weight: 5,
+                          dashArray: "",
+                          color: "#807c7c",
+                          fillColor: "#ffffff",
+                        });
+                      }
+                    },
+                    mouseout: (e) => {
+                      if (!isClicked) {
+                        const layer = e.target;
+                        layer.setStyle({
+                          fillOpacity: 0.7,
+                          weight: 3,
+                          dashArray: 3,
+                          color: "black",
+                          fillColor: "#000",
+                        });
+                      }
+                    },
+                  }}
+                />
+            );
+          })}
+          <div
+            className="sidebar"
+            style={{
+              right: sidebarIsOpen ? "0" : "-400px",
+            }}
+          >
+            <h2>Country Details</h2>
+            {/* <p>Country Name: {country.properties.name} </p> */}
+            <button onClick={handleSidebarClose}>Close Sidebar</button>
+          </div>
+        </MapContainer>
+      </div>
+    </div>
   );
 }
 
